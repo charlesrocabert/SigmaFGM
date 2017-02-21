3f94fff8282ef3cc6075e2198923f99958620f6d
Charles Rocabert <charlesrocabert@inria-evoevo1.insa-lyon.fr>
2017-02-21 15:10:00 +0100

    Removed useless files

--- example/old/make_movie.py
+++
@@ -1,5 +0,0 @@
-
-import os
-
-os.system("mencoder 'mf://tmp/_tmp*.png' -mf type=png:fps=10 -ovc lavc -lavcopts vcodec=wmv2 -oac copy -o animation.mpg")
-os.system("rm tmp/_tmp*.png")
--- example/run_solver.sh
+++ example/run_solver.sh
@@ -16,20 +16,20 @@
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #**********************************************************************
 
-../build/bin/run_solver\
--stabt        0\
--t            1000\
--seed         0\
--nbdim        2\
--nbparticles  1000\
--initmu       2.5\
--initsigma    1e-15\
--inittheta    0.0\
--dmu          0.0\
--dsigma       0.0\
--dtheta       0.0\
--statistics\
--oneaxis\
--nonoise\
--norotation\
-#-weightfitness\
+../build/bin/run_solver \
+-stabt        0 \
+-t            1000 \
+-seed         0 \
+-nbdim        1 \
+-nbparticles  10000 \
+-initmu       2.5 \
+-initsigma    1e-15 \
+-inittheta    0.0 \
+-dmu          0.01 \
+-dsigma       0.01 \
+-dtheta       0.01 \
+-statistics \
+#-oneaxis \
+#-nonoise \
+#-norotation \
+#-weightfitness \
